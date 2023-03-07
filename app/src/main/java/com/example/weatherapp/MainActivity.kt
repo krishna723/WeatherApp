e5edb59a2bfb43a8a357682e7e4da29a77a9d4d6
krishna723 <77148449+krishna723@users.noreply.github.com>
2023-03-07 20:12:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        val locationManager: LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+
+    }
+}
